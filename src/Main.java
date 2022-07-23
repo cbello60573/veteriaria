39dcc2bbadcc47c6fc32bac96bae1e8330773c72
Carlos B <carlosbello3317@hotmail.com>
2022-07-22 21:42:37 -0600

    Un hola mundo pa probar

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 
+import javax.swing.JOptionPane;
+
+
 public class Main {
 
     public static void main(String[] args) {
-  
+        JOptionPane.showMessageDialog(null, "Hola Mundo");
     }
 
 }
